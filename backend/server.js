9898b2502e998d415b7716a68b19f6b8d25ba27f
FelipeOPratinha <felipeaires98@hotmail.com>
2025-11-25 20:35:56 -0300

    [BACKEND] Alteração para aceitar qualquer IP de máquina apenas para desenvolvimento

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,11 @@ const db = require('./src/config/dbConfig');
 const port = process.env.APP_APPLICATION_PORT;
 
 db.sync().then(() => {
-    app.listen(port, () => {
+    app.get("/", (req, res) => {
+        res.send("API ON");
+    });
+
+    app.listen(port, "0.0.0.0", () => {
         console.clear();
         console.log('\x1b[32m%s\x1b[0m', 'Compiled successfully!', '\n');
         console.log(`Server is running on http://localhost:${port}`);
